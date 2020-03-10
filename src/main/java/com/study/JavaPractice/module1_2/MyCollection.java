f1a1514d8b8ac1fd141c109eef6ae0b3bd578ecf
Sergey Bodnar <sergeybodnar34@gmail.com>
2020-03-10 02:18:26 +0200

    Added Task_1 module_1_2.

---
+++ src/main/java/com/study/JavaPractice/module1_2/MyCollection.java
@@ -0,0 +1,41 @@
+package com.study.JavaPractice.module1_2;
+
+import java.util.*;
+
+class MyCollection extends ArrayList<Integer>{
+
+    public int getIndex(int index) {
+
+        return get(index);
+    }
+
+    @Override
+    public boolean add(Integer value) {
+        for (int i = 0; i < size(); i++) {
+            set(i, get(i) + value);
+        }
+        return super.add(value);
+    }
+
+    public boolean rem(Integer value) {
+        for (int j = 0; j < size(); j++) {
+            set(j, get(j) - value);
+        }
+        return super.remove(value);
+    }
+
+    public double getAverage(){
+        int sum = 0;
+        double avg;
+        for(int i = 0; i < size(); i++)
+            sum += getIndex(i);
+        avg = sum / size();
+        return avg;
+    }
+
+    public static int scan() {
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        return x;
+    }
+}
---
+++ src/main/java/com/study/JavaPractice/module1_2/Task_1.java
@@ -0,0 +1,76 @@
+package com.study.JavaPractice.module1_2;
+
+import java.util.*;
+
+public class Task_1 {
+    public static void main(String[] args) {
+
+        MyCollection myColl = new MyCollection();
+
+        System.out.print("Введите размер изначальной коллекции целых чисел, " +
+                "а затем и сами целые числа: ");
+        int count = MyCollection.scan();
+        for (int i = 0; i < count; i++)
+            myColl.add(i, MyCollection.scan());
+
+        System.out.println("Полученная коллекция целых чисел: \n" + myColl);
+        System.out.println();
+
+        int choice;
+        do {
+
+            System.out.println("Выберите операцию над коллекцией: \n" +
+                    "1 - добавление эл-та;\n" +
+                    "2 - удаление эл-та;\n" +
+                    "3 - поиск элемента по значению;\n" +
+                    "4 - поиск элемента по индексу;\n" +
+                    "5 - поиск максимального элемента;\n" +
+                    "6 - поиск минимального элемента;\n" +
+                    "7 - поиск среднего арифметического всех эл-ов;\n" +
+                    "0 - ничего не делать.\n");
+            System.out.print("Ваш выбор: ");
+
+            choice = MyCollection.scan();
+
+            System.out.println();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Введите добавляемый эл-нт: ");
+                    int a = MyCollection.scan();
+                    myColl.add(a);
+                    break;
+                case 2:
+                    System.out.print("Введите индекс удаляемого эл-нта: ");
+                    int b = MyCollection.scan();
+                    myColl.remove(b);
+                    myColl.rem(b);
+                    break;
+                case 3:
+                    System.out.print("Найти эл-нт по значению: ");
+                    int c = MyCollection.scan();
+                    System.out.println("Элемент <" + c + "> под индексом [" + myColl.indexOf(c) + "]");
+                    break;
+                case 4:
+                    System.out.println("Найти эл-нт под индексом: ");
+                    int d = MyCollection.scan();
+                    System.out.println("Под индексом [" + d + "] элемент <" + myColl.getIndex(d) + "]");
+                    break;
+                case 5:
+                    System.out.print("Максимальный эл-нт: " + Collections.max(myColl));
+                    break;
+                case 6:
+                    System.out.print("Минимальный эл-нт: " + Collections.min(myColl));
+                    break;
+                case 7:
+                    System.out.println("Среднее арифметическое всех эл-ов: " + myColl.getAverage());
+                    break;
+                default:
+                    break;
+            }
+            System.out.println("\nТекущая версия коллекции: \n" + myColl);
+            System.out.println();
+
+        } while ((choice < 8) && (choice > 0));
+    }
+}
